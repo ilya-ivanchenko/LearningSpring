3fab3f2e77f86be82d731cae2fb0c88ff3bc6031
ivanchenko92 <richiilya92@gmail.com>
2022-10-26 18:56:00 +0300

    application Calculator

--- springMVC/src/main/java/by/epam/ivanchenko/config/controller/FirstControl.java
+++ springMVC/src/main/java/by/epam/ivanchenko/config/controller/FirstControl.java
@@ -1,6 +1,7 @@
 package by.epam.ivanchenko.config.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +14,14 @@ public class FirstControl {
 
     @GetMapping("/hello")
 //    public String helloPage(HttpServletRequest request) {
-    public String helloPage(@RequestParam("name") String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 //        String name = request.getParameter("name");
 //        String surname = request.getParameter("surname");
+//        System.out.println("Hello, " + name + " " + surname);
 
-        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -26,5 +29,33 @@ public class FirstControl {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam("a") int a, @RequestParam("b") int b,
+                            @RequestParam("action") String action, Model calc) {
+
+        double result;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        calc.addAttribute("result", result);
+        return "first/calculator";
     }
 }

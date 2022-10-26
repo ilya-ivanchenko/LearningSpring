81b790efe640124211f928c5b6672d719cb4272b
ivanchenko92 <richiilya92@gmail.com>
2022-10-26 15:55:48 +0300

    mapping annotations and several controllers

--- springMVC/src/main/java/by/epam/ivanchenko/config/controller/FirstControl.java
+++ springMVC/src/main/java/by/epam/ivanchenko/config/controller/FirstControl.java
@@ -1,2 +1,19 @@
-package by.epam.ivanchenko.config.controller;public class FirstControl {
+package by.epam.ivanchenko.config.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")                // все адреса в этом классе будут иметь префикс  first, т.е. first/hello
+public class FirstControl {
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return"first/goodbye";
+    }
 }
--- springMVC/src/main/java/by/epam/ivanchenko/config/controller/SecondControl.java
+++ springMVC/src/main/java/by/epam/ivanchenko/config/controller/SecondControl.java
@@ -1,2 +1,12 @@
-package by.epam.ivanchenko.config.controller;public class SecondControl {
+package by.epam.ivanchenko.config.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondControl {
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
+    }
 }

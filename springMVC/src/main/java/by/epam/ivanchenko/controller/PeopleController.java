ba7304ae9c51073a7a29ef9f62a7e750135bfdf6
ivanchenko92 <richiilya92@gmail.com>
2022-10-27 14:49:37 +0300

    annotation modelattribute

--- springMVC/src/main/java/by/epam/ivanchenko/controller/PeopleController.java
+++ springMVC/src/main/java/by/epam/ivanchenko/controller/PeopleController.java
@@ -1,12 +1,11 @@
 package by.epam.ivanchenko.controller;
 
 import by.epam.ivanchenko.dao.PersonDAO;
+import by.epam.ivanchenko.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -29,5 +28,18 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model peopleModel) {      //извлекаем id из URL
         peopleModel.addAttribute("person", personDAO.show(id));                                                                     //получение одного чел по id и передача его на View
         return "people/show";
+    }
+
+    @GetMapping("/new")
+//    public String newPerson(Model peopleModel) {
+//        peopleModel.addAttribute("person", new Person());
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String createPeople(@ModelAttribute("person")Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }

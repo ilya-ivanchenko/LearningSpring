7ac80ec83b94e028f8b5a7a12f41ef65fb36d24c
ivanchenko92 <richiilya92@gmail.com>
2022-10-29 22:59:41 +0300

    Use JdbcTemplate

--- springMVC/src/main/java/by/epam/ivanchenko/config/SpringConfig.java
+++ springMVC/src/main/java/by/epam/ivanchenko/config/SpringConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("by.epam.ivanchenko")
@@ -46,5 +50,22 @@ public class SpringConfig implements WebMvcConfigurer {               // экв�
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+
+    @Bean
+    public DataSource dataSource() {                                                    //для JDBCTemplate созд. бин источника данных  - БД
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost/people");
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
--- springMVC/src/main/java/by/epam/ivanchenko/dao/PersonDAO.java
+++ springMVC/src/main/java/by/epam/ivanchenko/dao/PersonDAO.java
@@ -1,19 +1,23 @@
 package by.epam.ivanchenko.dao;
 
 import by.epam.ivanchenko.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component                                        // созд. бина
+@Component                                                              // созд. бина
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
+    private final JdbcTemplate jdbcTemplate;
 
-    private static final String URL = "jdbc:mysql://localhost/people";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "admin";
+    @Autowired                                                          // внедерение объекта JDBCTemplate с помощью конструктора в DAO
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     private static final String SHOW_ALL_USER = "SELECT * FROM person";
     private static final String ADD_PERSON = "INSERT INTO person VALUES(1, ?, ?, ?)";
@@ -21,109 +25,29 @@ public class PersonDAO {
     private static final String EDIT_PERSON = "UPDATE person SET name = ?, age = ?, email = ? WHERE id = ?";
     private static final String DELETE_PERSON = "DELETE FROM person WHERE  id = ?";
 
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();  //
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
     public List<Person> index() {
-        List<Person> people = new ArrayList<>();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(SHOW_ALL_USER);
-
-            while (resultSet.next()) {
-                Person person = new Person();
-
-                person.setId(resultSet.getInt("id"));
-                person.setName(resultSet.getString("name"));
-                person.setEmail(resultSet.getString("email"));
-                person.setAge(resultSet.getInt("age"));
-
-                people.add(person);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return people;
+//      return jdbcTemplate.query(SHOW_ALL_USER, new PersonMapper());
+//  т.к. в PersonMapper у объекта Person такие же поля, как и в таблице в БД, то можно использовть встроенный rowMapper:
+        return jdbcTemplate.query(SHOW_ALL_USER, new BeanPropertyRowMapper<>(Person.class));
     }
 
+/* Запросы в JDBCTemplate по умолчанию PreparedStatement
+*/
     public Person show(int id) {
-        Person person = null;
-        ResultSet resultSet;
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(FIND_PERSON);
-
-            preparedStatement.setInt(1, id);
-
-            resultSet = preparedStatement.executeQuery();
-
-            resultSet.next();
-
-            person = new Person();                                                          // получим только первого, если есть одинкаовые id
-            person.setId(resultSet.getInt("id"));
-            person.setName(resultSet.getString("name"));
-            person.setAge(resultSet.getInt("age"));
-            person.setEmail(resultSet.getString("email"));
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return person;
+      return jdbcTemplate.query(FIND_PERSON, new BeanPropertyRowMapper<>(Person.class), id).stream().findAny().orElse(null);
+// id - подставляем вместо ? массив объектов - так только в query. В update просто перечисляем
     }
-
+// stream().findAny().orElse(null)  - если будет хоть один объект Person, то он будет возвращен, если  строк не найдено - то возвращено null (может быть объект Error)
 
     public void save(Person person) {
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(ADD_PERSON);
-
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.setInt(2, person.getAge());
-            preparedStatement.setString(3, person.getEmail());
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace(); //
-        }
+         jdbcTemplate.update(ADD_PERSON, person.getName(), person.getAge(), person.getEmail());
     }
 
     public void update(int id, Person updatePerson) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(EDIT_PERSON);
-
-            preparedStatement.setString(1, updatePerson.getName());
-            preparedStatement.setInt(2, updatePerson.getAge());
-            preparedStatement.setString(3, updatePerson.getEmail());
-            preparedStatement.setInt(4, id);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace(); //
-        }
+       jdbcTemplate.update(EDIT_PERSON, updatePerson.getName(), updatePerson.getAge(), updatePerson.getEmail(), id);
     }
 
     public void delete(int id) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PERSON);
-
-            preparedStatement.setInt(1, id);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace(); //
-        }
+        jdbcTemplate.update(DELETE_PERSON, id);
     }
 }
